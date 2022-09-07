bb25d6d293a17c240e3ce66496e0baa97298e9ba
Denis Uraganov <github@uraganov.net>
2022-09-07 17:57:22 -0500

    jdk()

--- java/env.zsh
+++ java/env.zsh
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-# TODO ensure home location exists
-› export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
-=======
-<<<<<<< HEAD
  jdk() {
         version=$1
         export JAVA_HOME=$(/usr/libexec/java_home -v"$version");
@@ -10,8 +5,3 @@
  }
  
  jdk 1.8
-=======
-# TODO ensure home location exists
-› export JAVA_HOME="/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home"
->>>>>>> 2f2fa3213a333444fd003b3333112acaac56b817
->>>>>>> Stashed changes
